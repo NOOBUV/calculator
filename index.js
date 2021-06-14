ce11cf42c4e735dbdb5463226b658e8193eeceb5
Utkarsh Vijay <utkarshvijay99@gmail.com>
2021-06-15 00:54:25 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,44 @@
+let screen = document.getElementById('screen');
+let result=document.getElementById('calc')
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == '×') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            result.innerHTML="";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            result.innerHTML=screen.value;
+            screen.value = eval(screenValue);
+        }
+        else if (buttonText == '÷') {
+            buttonText = '/';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == '+') {
+            buttonText = '+';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == '−') {
+            buttonText = '-';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
