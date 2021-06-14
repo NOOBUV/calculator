7cc0b1a3eaddeb0923f9fce288c60b6e072d6ac1
Utkarsh Vijay <utkarshvijay99@gmail.com>
2021-06-15 03:25:01 +0530

    final update

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 let screen = document.getElementById('screen');
 let result=document.getElementById('calc')
 buttons = document.querySelectorAll('button');
-let screenValue = '';
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
         console.log('Button text is ', buttonText);
         if (buttonText == '×') {
             buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
+            screen.value += buttonText;
         }
         else if (buttonText == 'C') {
-            screenValue = "";
             result.innerHTML="";
-            screen.value = screenValue;
+            screen.value = "";
         }
         else if (buttonText == '=') {
             result.innerHTML=screen.value;
@@ -22,27 +19,22 @@ for (item of buttons) {
         }
         else if (buttonText == '÷') {
             buttonText = '/';
-            screenValue += buttonText;
-            screen.value = screenValue;
+            screen.value += buttonText;
         }
         else if (buttonText == '+') {
             buttonText = '+';
-            screenValue += buttonText;
-            screen.value = screenValue;
+            screen.value += buttonText;
         }
         else if (buttonText == '−') {
             buttonText = '-';
-            screenValue += buttonText;
-            screen.value = screenValue;
+            screen.value += buttonText;
         }
         else if (buttonText == '⋅') {
             buttonText='.';
-            screenValue+=buttonText;
-            screen.value=screenValue;
+            screen.value+=buttonText;
         }
         else {
-            screenValue += buttonText;
-            screen.value = screenValue;
+            screen.value += buttonText;
         }
 
     })
